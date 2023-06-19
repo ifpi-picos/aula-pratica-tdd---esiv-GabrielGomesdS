75e01f886648cf8d6be8a0ef25d7b5bc7043347a
GabrielGomesdS <capic.2021118tads0297@aluno.ifpi.edu.br>
2023-06-18 22:45:03 -0300

    commit 4 - adicionando currency

--- money.java
+++ money.java
@@ -1,18 +1,28 @@
 abstract class Money  {
-  protected int amount;
+ private String currency; 
+  protected String currency;
 
   public boolean equals(Object object)  {
     Money money = (Money) object;
     return amount == money.amount && getClass().equals(money.getClass());
   }  
 
-  abstract Money times(int multiplier);  
+  abstract Money times(int multiplier); 
+
+   String currency() {
+    return currency;
+  } 
 
   static Money dollar(int amount)  {
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   static Money franc(int amount) {
-    return new Franc(amount);
+     return new Franc(amount, "CHF");
+  }
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
   }
 }
